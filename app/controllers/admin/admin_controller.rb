524cdde583289d823e32109378daf241c5b55ddf
Hans Kurniadi <hans.d.k@live.com>
2017-03-17 18:28:52 +0000

    add nested review route and display reviews on a product

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,3 @@
-# FIXME bugs when already logged in asking to login again
 class Admin::AdminController < ApplicationController
   http_basic_authenticate_with name: ENV['username'], password: ENV['password']
 end
