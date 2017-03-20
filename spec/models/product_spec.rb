156d29cd5e1c16a936c833dfd628068727f7cd2e
Hans Kurniadi <hans.d.k@live.com>
2017-03-20 20:56:14 +0000

    add testing examples of all 4 validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Product, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
   describe 'Validations' do
+
     context "given all four fields set" do
       it "should save the new product successfully" do
         @category = Category.new({
@@ -17,6 +18,72 @@ RSpec.describe Product, type: :model do
             category_id: @category.id
           })
         expect(@product.save!).to be true
+      end
+    end
+
+    context "given all four fields set except 'name'" do
+      it "should not save the new product" do
+        @category = Category.new({
+            name: "Utility"
+          })
+        @category.save
+        @product = Product.new({
+            price: 100,
+            quantity: 12,
+            category_id: @category.id
+          })
+        expect{ @product.save! }.to raise_error(ActiveRecord::RecordInvalid)
+        # @product.errors.full_messages[0] returns "Name can't be blank"
+        expect(@product.errors.full_messages).to contain_exactly("Name can't be blank")
+      end
+    end
+
+    context "given all four fields set except 'price'" do
+      it "should not save the new product" do
+        @category = Category.new({
+            name: "Utility"
+          })
+        @category.save
+        @product = Product.new({
+            name: "Smart Bottle",
+            quantity: 12,
+            category_id: @category.id
+          })
+        expect{ @product.save! }.to raise_error(ActiveRecord::RecordInvalid)
+        # @product.errors.full_messages returns ["Price can't be blank", "Price cents is not a number", "Price is not a number"]
+        expect(@product.errors.full_messages).to include("Price can't be blank")
+      end
+    end
+
+    context "given all four fields set except 'quantity'" do
+      it "should not save the new product" do
+        @category = Category.new({
+            name: "Utility"
+          })
+        @category.save
+        @product = Product.new({
+            name: "Smart Bottle",
+            price: 100,
+            category_id: @category.id
+          })
+        expect{ @product.save! }.to raise_error(ActiveRecord::RecordInvalid)
+        expect(@product.errors.full_messages).to contain_exactly("Quantity can't be blank")
+      end
+    end
+
+    context "given all four fields set except 'category_id'" do
+      it "should not save the new product" do
+        @category = Category.new({
+            name: "Utility"
+          })
+        @category.save
+        @product = Product.new({
+            name: "Smart Bottle",
+            price: 100,
+            quantity: 12
+          })
+        expect{ @product.save! }.to raise_error(ActiveRecord::RecordInvalid)
+        expect(@product.errors.full_messages).to contain_exactly("Category can't be blank")
       end
     end
   end
