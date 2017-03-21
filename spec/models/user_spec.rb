4f53020c9175ceee55460eb59bb1424d1b7149e3
Hans Kurniadi <hans.d.k@live.com>
2017-03-21 04:52:08 +0000

    update gem file for heroku

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-# TODO refactor codes, DRY out your code
+
 RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   describe "Validations" do
